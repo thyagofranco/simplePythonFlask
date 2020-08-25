855a5eda4ebb3cb7ba9acf8bfdad6ec239305694
Júlio Rangel Ballot <julio.ballot@gmail.com>
2020-08-25 20:05:54 -0300

    updating __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_pyfile('_config.py')
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-Base = declarative_base()
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+#Base = declarative_base()
+#engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 
 from project.users.views import users_blueprint
 from project.courses.views import courses_blueprint
@@ -15,3 +15,6 @@ from project.courses.views import courses_blueprint
 
 app.register_blueprint(users_blueprint)
 app.register_blueprint(courses_blueprint)
+
+
+migrate = Migrate(app,db)
